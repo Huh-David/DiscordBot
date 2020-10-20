540d4fff8952feb0f5f8e5f57db435f0928495ca
David Huh <38101884+qt1337@users.noreply.github.com>
2020-10-20 13:53:58 +0200

    :sparkles: Added command to say hello.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import asyncio
+import random
+
 import discord
 import os
 
+from discord import Guild
 from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.voice_client import VoiceClient
@@ -12,6 +16,14 @@ client = commands.Bot(command_prefix='!')
 @client.event
 async def on_ready():
     print('Bot is online.\n\n')
+
+
+# Private Methods
+
+# Commands
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hi")
 
 
 client.run(TOKEN)
